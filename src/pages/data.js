249492c4789b7473bd77a6b18a36f08d8a4a1cd4
ajmalameen <ajmalameenkm10@gmail.com>
2023-01-12 13:56:01 +0530

    final deploy

---
+++ src/pages/data.js
@@ -0,0 +1,77 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://wallpapercave.com/wp/wp9588350.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://wallpapercave.com/wp/wp9588350.jpg",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://wallpapercave.com/wp/wp9588350.jpg",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://wallpapercave.com/dwp2x/wp6203218.jpg",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://wallpapercave.com/dwp2x/wp6203269.jpg",
+      title: "LOUNGEWEAR T-S",
+    },
+    {
+      id: 3,
+      img: "https://wallpapercave.com/wp/wp9588589.jpg",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://nb.scene7.com/is/image/NB/bb550sn1_nb_02_i?$pdpflexf22x$&qlt=80&fmt=webp&wid=944&hei=944",
+    },
+    {
+      id:2,
+      img:"https://www.pngarts.com/files/1/Jacket-PNG-Image-Background.png",
+    },
+    {
+      id:3,
+      img:"https://www.pngarts.com/files/1/Jeans-Jacket-Transparent-Background-PNG.png",
+    },
+    {
+      id:4,
+      img:"https://www.pngarts.com/files/3/Hooded-Jacket-Men-PNG-Download-Image.png",
+    },
+    {
+      id:5,
+      img:"https://nb.scene7.com/is/image/NB/ms327cnw_nb_05_i?$pdpflexf22x$&qlt=80&fmt=webp&wid=944&hei=944",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://nb.scene7.com/is/image/NB/mr993gl_nb_02_i?$pdpflexf2$&qlt=80&wid=880&hei=880",
+    },
+    {
+      id:8,
+      img:"https://cdn.shopify.com/s/files/1/0281/0635/8862/products/Origins_800x.png?v=1643821146",
+    },
+  ]
